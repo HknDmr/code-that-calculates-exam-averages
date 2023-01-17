17448500e00e8873811faf10911ed4be90a7f80e
Hakan <hkn-demir@hotmail.com>
2023-01-17 12:24:14 +0200

    Code Format Fixed

--- src/Main.java
+++ src/Main.java
@@ -15,35 +15,33 @@ public class Main {
         //If you have different percentage calculations for the exams, you can change them.
         average= (quiz*0.2) + (firstExam*0.3) + (secondExam*0.5);
         System.out.println("Your average score is : " + average);
-        String score = (average >=50) ? "You Passed" : "You Failed";
-        System.out.println(score);
 
         if (average <=29){
-            System.out.println("Your Letter Grade DD");
+            System.out.println("Your Letter Grade \"FF\" " + "You Failed The Lesson");
         }
         else if (average <=39){
-            System.out.println("Your Letter Grade FD");
+            System.out.println("Your Letter Grade \"FD\" " + "You Failed The Lesson");
         }
         else if (average <=49){
-            System.out.println("Your Letter Grade DD");
+            System.out.println("Your Letter Grade \"DD\" " + "You Passed The Lesson as a Responsible");
         }
         else if (average <=59){
-            System.out.println("Your Letter Grade DC");
+            System.out.println("Your Letter Grade \"DC\" " + "You Passed The Lesson as a Responsible");
         }
         else if (average <=69){
-            System.out.println("Your Letter Grade CC");
+            System.out.println("Your Letter Grade \"CC\" " + "You Passed The Lesson");
         }
         else if (average <=74){
-            System.out.println("Your Letter Grade CB");
+            System.out.println("Your Letter Grade \"CB\" " + "You Passed The Lesson");
         }
         else if (average <=79){
-            System.out.println("Your Letter Grade BB");
+            System.out.println("Your Letter Grade \"BB\" " + "You Passed The Lesson");
         }
         else if (average <=89){
-            System.out.println("Your Letter Grade BA");
+            System.out.println("Your Letter Grade \"BA\" " + "You Passed The Lesson");
         }
         else if (average >=90){
-            System.out.println("Your Letter Grade AA");
+            System.out.println("Your Letter Grade \"AA\" " + "You Passed The Lesson With The Highest Letter Grade");
         }
     }
 }
